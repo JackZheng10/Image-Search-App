36eda8c02f5468158661cb51a4c8f77295e57842
Jack Zheng <jackzheng10@yahoo.com>
2019-11-26 21:38:49 -0500

    Added search bar input support when pressing Enter key

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange() {}
+  state = { term: "" };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onFormSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={event => this.setState({ term: event.target.value })}
+            />
           </div>
         </form>
       </div>
