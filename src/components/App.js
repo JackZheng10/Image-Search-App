5462a9cda89649bb87cf8fa410dccea7f28509b1
Jack Zheng <jackzheng10@yahoo.com>
2019-12-23 03:53:06 -0500

    Added Unsplash API request to search for a term

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Axios from "axios";
 import SearchBar from "./SearchBar.js";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    Axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 6b564aa5706ab063f170e8ac2f14a7dc674d8b3563ed58b8026df47f1d4edc0f"
+      }
+    });
   }
 
   render() {
